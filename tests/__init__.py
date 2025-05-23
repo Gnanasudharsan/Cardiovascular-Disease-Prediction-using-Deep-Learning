d2c33563425521cebbbd6c217b99ab918891322b
Gnanasudharsan Ashokumar <84964314+Gnanasudharsan@users.noreply.github.com>
2025-05-23 22:54:44 +0530

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+"""
+Test package for cardiovascular disease prediction
+
+Contains unit tests for all modules:
+- Data preprocessing tests
+- Model tests
+- Feature selection tests
+- Evaluation metrics tests
+- Utility tests
+"""
+
+__version__ = "1.0.0"
